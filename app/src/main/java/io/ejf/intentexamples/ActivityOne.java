67f5a2b64bbbf6c9c3c990922b938718f72ddbc9
E John Feig <ejohn@feigdev.com>
2015-11-03 09:43:55 -0800

    added another intent

--- app/src/main/java/io/ejf/intentexamples/ActivityOne.java
+++ app/src/main/java/io/ejf/intentexamples/ActivityOne.java
@@ -5,21 +5,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ActivityOne extends AppCompatActivity {
-    Button launchIntent;
+    private TextView tv;
+    private Button launchIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_one);
         launchIntent = (Button) findViewById(R.id.launch_intent);
+        tv = (TextView) findViewById(R.id.activity_one_text);
+
         launchIntent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 launchIntent();
             }
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Intent intent = getIntent();
+        if (intent.hasExtra("EXTRA_CONTENT"))
+            tv.setText(tv.getText() + " " + intent.getExtras().getString("EXTRA_CONTENT"));
     }
 
     private void launchIntent() {
--- app/src/main/java/io/ejf/intentexamples/ActivityTwo.java
+++ app/src/main/java/io/ejf/intentexamples/ActivityTwo.java
@@ -1,17 +1,33 @@
 package io.ejf.intentexamples;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by ejf3 on 11/2/15.
  */
 public class ActivityTwo extends AppCompatActivity {
+    private Button launchIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two);
+        launchIntent = (Button) findViewById(R.id.launch_intent);
+        launchIntent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchIntent();
+            }
+        });
+    }
 
+    private void launchIntent() {
+        Intent intent = new Intent(this, ActivityOne.class);
+        intent.putExtra("EXTRA_CONTENT", "saw ActivityTwo");
+        startActivity(intent);
     }
 }
